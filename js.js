2387b5c3bc7ffc55c40c7ea08207d46fab008f85
tesfaye <contactesfaye>
2023-03-15 17:00:48 +0300

    testing made

---
+++ js.js
@@ -0,0 +1,10 @@
+
+function letterCount(word) {
+    var word;
+    var length = word.length;
+
+    return length;
+}
+console.log(letterCount('Hello'));
+
+module.exports = letterCount 
---
+++ js.test.js
@@ -0,0 +1,9 @@
+const letterCount = require('./js')
+
+test('counts the length of a word', () => {
+   expect(letterCount('hello')).toBe(5)
+})
+
+
+
+
